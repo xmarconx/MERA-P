8c5918fbb2c179c67fc07233ef613068ed9c0048
Alaaddin Turgay <61379434+xmarconx@users.noreply.github.com>
2022-12-24 17:49:28 +0300

    Kalan sayfalar eklendi

--- routes/photoRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import * as photoController from "../controllers/photoController.js";
-
-const router = express.Router();
-
-router.route("/").post(photoController.createPhoto);
-
-export default router;
