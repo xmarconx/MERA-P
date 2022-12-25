374c7a8a380aabaf1819114722ca4482164a70bf
Alaaddin Turgay <61379434+xmarconx@users.noreply.github.com>
2022-12-25 14:32:00 +0300

    Yeni Sayfalar
    Yeni Sayfalar ile beraber var olanlar düzenlendi.

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ import newsRouter from "./routes/news.js";
 import sportsRouter from "./routes/sports.js";
 import searchRouter from "./routes/searchRoute.js";
 import healthRouter from "./routes/health.js";
+import businessRouter from "./routes/business.js";
+import scienceRouter from "./routes/science.js";
+import technologyRouter from "./routes/technology.js";
+import generalRouter from "./routes/general.js";
+import entertainmentRouter from "./routes/entertainment.js";
+//import categoriesRouter from "./routes/categories.js";
 
 dotenv.config();
 
@@ -27,14 +33,27 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", pageRoute);
 
-
 app.use('/news', newsRouter);
 
 app.use('/sports', sportsRouter);
 
 app.use('/search', searchRouter);
 
+app.use('/business', businessRouter);
+
 app.use('/health', healthRouter);
+
+app.use('/science', scienceRouter);
+
+app.use('/sports', sportsRouter);
+
+app.use('/technology', technologyRouter);
+
+app.use('/general', generalRouter);
+
+app.use('/entertainment', entertainmentRouter);
+
+//app.use('/categories', categoriesRouter);
 
 
 app.listen(port, () => {
